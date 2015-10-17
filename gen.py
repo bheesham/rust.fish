3370d6dfd71519dff1ae559b427533711a1e25c7
Bheesham Persaud <bheesham.persaud@live.ca>
2015-10-17 00:25:23 -0400

    Now using the fish string funcs to gen help msgs
    With the addition of the `string` function in fish, I was able
    to get rid of a lot of lines from the Python generator script.

--- gen.py
+++ gen.py
@@ -9,84 +9,9 @@ env = Environment(loader=FileSystemLoader('.'))
 rust_config = env.get_template('rustc.fish.template')
 cargo_config = env.get_template('cargo.fish.template')
 
-
-# Codegen
-codegen_re = re.compile('(\s+)-C(?P<arg>.+)(\s+)--(\s+)(?P<desc>[^\n]+)')
-codegen_out, _ = subprocess.Popen(
-	['rustc','-C', 'help'],
-	stdout=subprocess.PIPE).communicate()
-
-codegens = []
-for match in codegen_re.finditer(codegen_out):
-	arg = match.group('arg')
-	if '=' in arg:
-		arg = arg[:arg.index('=')+1]
-	
-	codegens.append({
-		'arg': arg.strip(),
-		'desc': match.group('desc').strip()
-	})
-
-# Warnings
-warning_re = re.compile('(\s+)(?P<arg>.+)(\s+)(allow|warn|deny)(\s+){2}(?P<desc>[^\n]+)')
-warning_out, _ = subprocess.Popen(
-	['rustc','-W', 'help'],
-	stdout=subprocess.PIPE).communicate()
-
-warning_arguments = []
-for match in warning_re.finditer(warning_out):
-	warning_arguments.append({
-		'arg': match.group('arg').strip(),
-		'desc': match.group('desc').strip()
-	})
-
-warnings = []
-warning_flags = [
-	{
-		'short': 'W',
-		'long': 'warn'
-	},
-	{
-		'short': 'A',
-		'long': 'allow'
-	},
-	{
-		'short': 'D',
-		'long': 'deny'
-	},
-	{
-		'short': 'F',
-		'long': 'forbid'
-	}
-]
-
-for flag in warning_flags:
-	for arg in warning_arguments:
-		warnings.append({
-			'short': flag['short'], 'long': flag['long'], 'arg': arg['arg'],
-			'desc': arg['desc']
-		})
-
-
-# Internal debugging
-debug_re = re.compile('(\s+)-Z(?P<arg>.+)(\s+)--(\s+)(?P<desc>[^\n]+)')
-debug_out, _ = subprocess.Popen(
-		['rustc','-Z', 'help'],
-		stdout=subprocess.PIPE).communicate()
-
-debugs = []
-for match in debug_re.finditer(debug_out):
-	debugs.append({
-		'arg': match.group('arg').strip(),
-		'desc': match.group('desc').strip()
-	})
-
 with open('rustc.fish', 'w') as handle:
 	handle.write(rust_config.render(
-		r='complete -c rustc',
-		codegens=codegens,
-		warnings=warnings,
-		debugs=debugs
+		r='complete -c rustc'
 	))
 	handle.write('\n')
 
